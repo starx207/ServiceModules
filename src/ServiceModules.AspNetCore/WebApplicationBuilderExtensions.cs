ceb3e5739007dec942dfe642eab2ebb8f026c7ed
Andrew Starr <astarr@appraisalresearch.cc>
2022-03-22 14:30:12 -0400

    Move IRegistryModule and Abstract to separate assembly so they can be consumed without the other dependencies

--- src/ServiceModules.AspNetCore/WebApplicationBuilderExtensions.cs
+++ src/ServiceModules.AspNetCore/WebApplicationBuilderExtensions.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Builder;
 using ServiceModules.AspNetCore;
-using System.Reflection;
 
 namespace ServiceModules;
 public static class WebApplicationBuilderExtensions {
+    /// <summary>
+    /// Applies the <see cref="IRegistryModule"/>s from the given assemblies.
+    /// If no assemblies provided, the entry assembly will be scanned for <see cref="IRegistryModule"/> implementations to apply.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="assemblies">The assemblies to scan</param>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException">
+    /// When unable to instantiate an <see cref="IRegistryModule"/> implementation.
+    /// </exception>
     public static void ApplyModules(this WebApplicationBuilder builder, params Assembly[] assemblies)
         => builder.ApplyModules(config => {
             if (assemblies.Any()) {
@@ -12,17 +22,17 @@ public static class WebApplicationBuilderExtensions {
         });
 
     /// <summary>
-    /// Applies the <see cref="IRegistryModule"/>s according to the <see cref="ServiceCollectionModuleConfiguration"/>.
-    /// If no configuration provided, searches the entry assembly for <see cref="IRegistryModule"/> implementations and runs them.
+    /// Applies the <see cref="IRegistryModule"/>s according to the <see cref="WebApplicationModuleConfiguration"/>.
+    /// If the configuration does not specify any modules, module types, or module assemblies, 
+    /// the entry assembly will be scanned for <see cref="IRegistryModule"/> implementations to apply.
     /// </summary>
     /// <param name="builder"></param>
     /// <param name="moduleConfiguration">Configuration for which modules to apply</param>
     /// <returns></returns>
     /// <exception cref="InvalidOperationException">
-    /// When the provided host environment does not implement <see cref="IHostEnvironment"/>,
-    /// or when unable to instantiate an <see cref="IRegistryModule"/> implementation.
+    /// When unable to instantiate an <see cref="IRegistryModule"/> implementation.
     /// </exception>
-    public static void ApplyModules(this WebApplicationBuilder builder, Action<WebApplicationModuleConfiguration> moduleConfiguration) 
+    public static void ApplyModules(this WebApplicationBuilder builder, Action<WebApplicationModuleConfiguration> moduleConfiguration)
         => builder.Services.ApplyModules(config => {
             config.WithEnvironment(builder.Environment)
             .WithConfiguration(builder.Configuration);
